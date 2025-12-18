c4b475fc457512ffc83221adb9f2dd27da191800
eigmax <sd@eigen.market>
2025-12-18 12:36:08 +0000

    feat: add proof api

--- proof-builder-rpc/src/api/proof_handler.rs
+++ proof-builder-rpc/src/api/proof_handler.rs
@@ -6,15 +6,12 @@ use crate::api::proofs::{
 use crate::api::response::{ApiErrorExt, ApiResult, ok_response};
 use crate::api::validation::InputValidator;
 use crate::task::{
-    add_operator_task, add_watchtower_task, current_time_secs, find_operator_task,
-    find_watchtower_task,
+    add_operator_task, add_watchtower_task, find_operator_task, find_watchtower_task,
 };
-use anyhow::Context;
 use axum::Json;
 use axum::extract::{Query, State};
-use std::fs;
 use std::sync::Arc;
-use store::{OperatorProof, ProofState, WatchtowerProof};
+use store::ProofState;
 use tracing::info;
 
 #[axum::debug_handler]
@@ -53,19 +50,21 @@ pub(super) async fn get_chain_proof_task(
             };
             let has_prev_proof = storage_process
                 .find_long_running_task_proof_including_block_number(
-                    proof.block_start -1,
+                    proof.block_start - 1,
                     payload.proof_type.get_chain_name().to_string(),
                 )
                 .await
-                .api_error("GET_CHAIN_PROOF_ERROR")?.is_some();
+                .api_error("GET_CHAIN_PROOF_ERROR")?
+                .is_some();
             let has_next_proof = storage_process
                 .find_long_running_task_proof_including_block_number(
                     proof.block_end,
                     payload.proof_type.get_chain_name().to_string(),
                 )
                 .await
-                .api_error("GET_CHAIN_PROOF_ERROR")?.is_some();
-            
+                .api_error("GET_CHAIN_PROOF_ERROR")?
+                .is_some();
+
             ok_response(ChainProofDescResponse {
                 proof_desc: Some(ChainProofDesc {
                     block_start: proof.block_start,
@@ -77,9 +76,9 @@ pub(super) async fn get_chain_proof_task(
                     proving_cycles: proof.cycles,
                     proving_time: proof.proving_time,
                     total_time_to_proof,
-                    proof_size: 0.0, // todo
+                    proof_size: proof.proof_size as f64 / 1000.0, // use KiB
                     zkm_version: proof.zkm_version,
-                    pub_values: "".to_string(), // todo
+                    pub_values: proof.public_value_hex.unwrap_or("".to_string()),
                     has_prev_proof,
                     has_next_proof,
                     created_at: proof.created_at,
@@ -140,11 +139,11 @@ pub(super) async fn post_watchtower_proof_task(
     let instance_id = InputValidator::validate_uuid(&payload.instance_id, "instance_id")?;
     let graph_id = InputValidator::validate_uuid(&payload.graph_id, "graph_id")?;
     let challenge_txid =
-        InputValidator::validate_btc_txid(&payload.challenge_txid, "challenge_txid")?.into();
+        InputValidator::validate_btc_txid(&payload.challenge_txid, "challenge_txid")?.to_string();
     let challenge_init_txid =
         InputValidator::validate_btc_txid(&payload.challenge_init_txid, "challenge_init_txid")?
-            .into();
-    let mut storage_process =
+            .to_string();
+    let storage_process =
         api_state.local_db.acquire().await.api_error("POST_WATCHTOWER_PROOF_TASK_ERROR")?;
 
     let watchtower_proofs = find_watchtower_task(&api_state.local_db, instance_id, graph_id)
