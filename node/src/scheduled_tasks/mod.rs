3393c352b8b889faaabfadd143dc8a47af7d733e
jackDev221 <jackDev221@126.com>
2025-12-19 09:32:02 +0800

    fix create_watchtower_proof

--- node/src/scheduled_tasks/mod.rs
+++ node/src/scheduled_tasks/mod.rs
@@ -179,3 +179,9 @@ fn get_timestamp_from_contract_data(input: &[u8; 32]) -> i64 {
     timestamp_bytes.copy_from_slice(&input[24..32]);
     i64::from_be_bytes(timestamp_bytes)
 }
+
+
+#[test]
+fn dd(){
+    println!("{}", uuid::Uuid::new_v4());
+}
