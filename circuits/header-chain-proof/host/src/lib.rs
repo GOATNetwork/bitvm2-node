94c432578d9a130b680f84801cf933c8d415ab39
eigmax <sd@eigen.market>
2025-12-23 07:49:57 +0000

    fix: workaround testnet3

--- circuits/header-chain-proof/host/src/lib.rs
+++ circuits/header-chain-proof/host/src/lib.rs
@@ -5,7 +5,6 @@ use header_chain::{
     BlockHeaderCircuitOutput, CircuitBlockHeader, HeaderChainCircuitInput, HeaderChainPrevProofType,
 };
 use proof_builder::{LongRunning, ProofBuilder, ProofRequest};
-use rand::Rng;
 use sha2::{Digest, Sha256};
 use std::{
     fs,
@@ -102,21 +101,10 @@ pub async fn fetch_header_chain(
 
     writer.seek(std::io::SeekFrom::Start((block_headers.len() * 80) as u64))?;
 
-    let mut cnt: usize = 0;
-    let mut range = 500;
-    let mut rng = rand::thread_rng();
     for i in start..(start + batch_size) {
         tracing::info!("get block by height: {i}");
         let block = btc_client.get_block_by_height(i as u32).await?;
         tracing::info!("block_id {i}: {}", block.block_hash().to_string());
-
-        cnt += 1;
-        if cnt.is_multiple_of(range) {
-            range = rng.gen_range(500..1000);
-            let ms = rng.gen_range(500..1000);
-            tokio::time::sleep(tokio::time::Duration::from_millis(ms)).await;
-        }
-
         let header: header_chain::CircuitBlockHeader = block.header.into();
         block_headers.push(header.clone());
         header.serialize(&mut writer)?;
