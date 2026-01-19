6c7ea55683193b9e876a70b77f3799700200fbf8
eigmax <sd@eigen.market>
2026-01-19 02:52:04 +0000

    fix: remove message broadcast

--- circuits/cron-header-chain-proof.sh
+++ circuits/cron-header-chain-proof.sh
@@ -12,7 +12,7 @@ batch=${2:-$_batch}
 function find_input_proof() {
   local start="$1"
   local input_file
-  input_file=$(find $DATA -maxdepth 1 -type f -name '*-*.bin' -printf '%f\n' |
+  input_file=$(find $DATA -maxdepth 1 -type f -regex '.*[0-9]+-[0-9]+\.bin$' -printf '%f\n' |
     awk -v sum="$start" -F '[-.]' '($1 + $2) == sum { print $0; exit }')
 
   if [ ! $input_file ]; then
@@ -31,6 +31,7 @@ else
 fi
 
 echo "Start i=$start, batch=$batch"
+exit
 
 while true; do
   echo "Running for i=$start"
