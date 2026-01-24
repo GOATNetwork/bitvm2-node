4e5da39a6289eb799beee547cc7dc05991489d77
eigmax <sd@eigen.market>
2026-01-24 06:09:45 +0000

    fix: access invalid memory

--- crates/bitvm2-ga/src/types.rs
+++ crates/bitvm2-ga/src/types.rs
@@ -632,11 +632,14 @@ pub mod node_serializer {
             cursor += NUM_U256;
 
             // FIXME: this is a tricky way to handle Wots16: if we use ? modifier, this will raise SEGV.
-            let pk22 = extract_wots_pubkeys::<Wots16, NUM_HASH, D::Error>(
+            let pk22 = match extract_wots_pubkeys::<Wots16, NUM_HASH, D::Error>(
                 &all,
                 cursor,
                 "groth16pk.wots_hash",
-            )?;
+            ) {
+                Err(e) => return Err(e),
+                Ok(pk) => pk,
+            };
             Ok((pk0, pk1, Box::new((pk20, pk21, pk22))))
         }
     }
