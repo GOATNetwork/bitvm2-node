c0e4be8d8a1306f53c940af474b3cb5b063b286e
eigmax <sd@eigen.market>
2026-01-26 02:50:47 +0000

    fix: lint

--- node/src/utils.rs
+++ node/src/utils.rs
@@ -1723,8 +1723,7 @@ pub async fn get_watchtower_commitment(
             (graph.challenge_txid, graph.watchtower_challenge_init_txid)
     {
         // check if challenge_txid is confirmed
-        let txid = Txid::from(challenge_txid.0);
-        let tx_status = btc_client.get_tx_status(&txid).await?;
+        let tx_status = btc_client.get_tx_status(&challenge_txid.0).await?;
         if !tx_status.confirmed {
             warn!("graph {graph_id} challenge tx is not confirmed");
             return Ok((None, get_watchtower_proof_wait_secs()));
