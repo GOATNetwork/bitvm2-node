4d58a174f4ad98610ebcf65391041000670fb89e
KSlashh <48985735+KSlashh@users.noreply.github.com>
2026-04-21 16:52:13 +0800

    fix subgraph query error

--- node/src/scheduled_tasks/event_watch_task.rs
+++ node/src/scheduled_tasks/event_watch_task.rs
@@ -977,9 +977,16 @@ pub async fn fetch_history_events(
                 .await
             {
                 Ok(Some(v)) => v,
-                Ok(None) | Err(_) => {
+                Ok(None) => {
                     warn!(
-                        "fetch_history_events:fail to get graph sync block height, will try later"
+                        "fetch_history_events:fail to get graph sync block height, will try later, empty value returned"
+                    );
+                    sleep(Duration::from_millis(500)).await;
+                    continue;
+                }
+                Err(e) => {
+                    warn!(
+                        "fetch_history_events:fail to get graph sync block height, will try later, err:{e:#}"
                     );
                     sleep(Duration::from_millis(500)).await;
                     continue;
@@ -1092,8 +1099,14 @@ pub async fn monitor_events_item(
     let current_finalized =
         match query_client.get_sync_block_height(&watch_contract.the_graph_url).await {
             Ok(Some(v)) => v,
-            Ok(None) | Err(_) => {
-                warn!("monitor_events_item:fail to get graph sync block height, will try later");
+            Ok(None) => {
+                warn!("monitor_events_item:fail to get graph sync block height, will try later, empty value returned");
+                return Ok(());
+            },
+            Err(e) => {
+                warn!(
+                    "monitor_events_item:fail to get graph sync block height, will try later, err:{e:#}"
+                );
                 return Ok(());
             }
         };
