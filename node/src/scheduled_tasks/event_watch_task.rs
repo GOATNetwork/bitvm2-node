8594382c131f5741006948f5efe4b6fbcd9d51d4
KSlashh <48985735+KSlashh@users.noreply.github.com>
2026-04-21 17:13:26 +0800

    fmt

--- node/src/scheduled_tasks/event_watch_task.rs
+++ node/src/scheduled_tasks/event_watch_task.rs
@@ -1096,20 +1096,24 @@ pub async fn monitor_events_item(
     .await?;
     let query_client = GraphQueryClient::new();
     // let current_finalized = goat_client.get_finalized_block_number().await?;
-    let current_finalized =
-        match query_client.get_sync_block_height(&watch_contract.the_graph_url).await {
-            Ok(Some(v)) => v,
-            Ok(None) => {
-                warn!("monitor_events_item:fail to get graph sync block height, will try later, empty value returned");
-                return Ok(());
-            },
-            Err(e) => {
-                warn!(
-                    "monitor_events_item:fail to get graph sync block height, will try later, err:{e:#}"
-                );
-                return Ok(());
-            }
-        };
+    let current_finalized = match query_client
+        .get_sync_block_height(&watch_contract.the_graph_url)
+        .await
+    {
+        Ok(Some(v)) => v,
+        Ok(None) => {
+            warn!(
+                "monitor_events_item:fail to get graph sync block height, will try later, empty value returned"
+            );
+            return Ok(());
+        }
+        Err(e) => {
+            warn!(
+                "monitor_events_item:fail to get graph sync block height, will try later, err:{e:#}"
+            );
+            return Ok(());
+        }
+    };
 
     if watch_contract.from_height == 0 || watch_contract.from_height >= current_finalized {
         warn!(
